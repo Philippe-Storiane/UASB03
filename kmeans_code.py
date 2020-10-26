ee2e94e9e21c724842f7648b0a83b384f636c406
Philippe-Storiane <45934948+Philippe-Storiane@users.noreply.github.com>
2020-10-26 06:09:26 +0100

    Add files via upload

--- kmeans_code.py
+++ kmeans_code.py
@@ -9,8 +9,10 @@ import numpy as np
 import pandas as pd
 
 from sklearn.cluster import MiniBatchKMeans
-from sklearn.metrics import pairwise_distances_argmin_min
+#from sklearn.metrics import pairwise_distances_argmin_min
 
+CLUSTER_SIZE = 10
+REPRESENTATIVE=10
 
 data1 = pd.read_csv( "embedding00.txt", header=None)
 data2 = pd.read_csv( "embedding01.txt", header=None)
@@ -22,6 +24,8 @@ labels2 = data2[ 0 ]
 labels3 = data3[ 0 ]
 labels4 = data4[ 0 ]
 
+labels = labels1.append( [ labels2, labels3, labels4], ignore_index= True)
+
 embeddings1 = data1.iloc[:, 1:]
 embeddings2 = data2.iloc[:, 1:]
 embeddings3 = data3.iloc[:, 1:]
@@ -32,19 +36,20 @@ np_embeddings2 = embeddings2.to_numpy()
 np_embeddings3 = embeddings3.to_numpy()
 np_embeddings4 = embeddings4.to_numpy()
 
-np_embeddings = np.concatenate( (np_embeddings1)) #, np_embeddings2, np_embeddings3, np_embeddings4))
+np_embeddings = np.concatenate( (np_embeddings1, np_embeddings2, np_embeddings3, np_embeddings4), axis=0) #, np_embeddings2, np_embeddings3, np_embeddings4))
 
-kmeans = MiniBatchKMeans( 10 ).fit( np_embeddings )
+# use cosine distance by normalizing embeddings
+length = np.sqrt( np_embeddings ** 2).sum(axis = 1)[:,None]
+np_embeddings = np_embeddings / length
 
-close = pairwise_distances_argmin_min(kmeans.cluster_centers_, x, metric='euclidean')
-index_closest_points = close[0]
-distance_closest_points = close[1]
+kmeans = MiniBatchKMeans( CLUSTER_SIZE ).fit( np_embeddings )
 
-for i in range(0, K):
-    print("The closest word to the centroid of class {0} is {1}, the distance is {2}".format(i, labels[index_closest_points[i]], distance_closest_points[i]))
-
-
-
-
-
-
+distances = kmeans.transform( np_embeddings )
+for cluster in range(CLUSTER_SIZE):
+    distance = distances[:, cluster]
+    closest_indices = np.argsort( distance)[: REPRESENTATIVE]
+    print( "cluster " + str( cluster))
+    for indice in closest_indices:
+        print( '%s (%f)' % (labels[ indice] ,distance[ indice]))
+    
+    
