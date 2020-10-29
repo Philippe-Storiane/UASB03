da71417a24de25186972439f9f20ad4b0ebbfe9f
Philippe-Storiane <45934948+Philippe-Storiane@users.noreply.github.com>
2020-10-29 06:07:27 +0100

    Add files via upload

--- word2vec_load.py
+++ word2vec_load.py
@@ -35,20 +35,29 @@ i = 0
 method_file = open(raw_text_filename, encoding="utf-8")
 method_features = method_file.readlines()
 t1 = datetime.datetime.now()
+text = []
 for method_feature in method_features:
 #    first_space = method_feature.find(" ")
 #    second_space = method_feature.find(" ", first_space + 1)
 #    methods_words = nlp( method_feature[ second_space + 1: -1])
     data = method_feature.split()
-    method_words = nlp( " ".join(data[2:])) 
-    i = i  + 1   
-    if (i > 5000):
-        break
-    if ( i % 1000) == 0:
-        t2 = datetime.datetime.now()
-        print("line " + str(i))
-        print( t2 - t1)
-        t1 = t2
-#    for method_word in methods_words:
-#        if ( method_word.pos_ == 'NUM'):
+    words =  " ".join(data[2:]) 
+    class_name = data[0]
+    text.append(( words, { 'className': class_name}))
 #            print(method_word.lemma_)
+
+def part_of_speech( text, pos):
+    t1 = datetime.datetime.now()
+    docs = nlp.pipe( text[0:20000], as_tuples=True)
+    parts = {}
+    for doc,context in docs:
+        for tok in doc:
+            if  tok.pos_ == pos:
+                if tok.text in parts:
+                    parts[ tok.text ] += 1
+                else:
+                    parts[ tok.text ] = 1
+    t2 = datetime.datetime.now()
+    print ( t2 - t1)
+    return parts
+
